57b93086b8aae2800216250beee7b73f1b763a5a
Santiago <a01660035@tec.mx>
2025-04-21 20:19:24 -0600

    Actualizacion de API con base de datos

--- static/node-mysql-api/database.js
+++ static/node-mysql-api/database.js
@@ -13,30 +13,31 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ["capacitatec.whirlpool.com"], // Allow requests only from your frontend
+    origin: ["chttp://zwwk4ocg8k0ko4g08wkgoo00.4.172.252.35.sslip.io"], // Allow requests only from your frontend
     methods: ["POST", "GET"],
+    credentials: true, 
     allowedHeaders: ["Content-Type"]
 }));
 
 // Session config
 app.use(session({
-    secret: "cookiesuser",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: false, // https --> true
-      domain: ".capacitatec.whirlpool.com",
-      maxAge: 1000 * 60 * 60 // 1 hora
-    }
-  }));
+  secret: "cookiesuser",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: false, // true si usas HTTPS
+    maxAge: 1000 * 60 * 60
+  }
+}));
 
 // SQL DB Connection
 const pool = mysql.createConnection({
-    host: "localhost",
+    host: "4.172.252.35",
     user: "root",
-    password: "F4ng.Sn0o7",
-    database: "webcap_schema",
+    password: "Ht1EHtaeYopyicq9MeXa1CDTaqz0lXzEh0F7ZIifA69tPN8600YzfrtX5FfzsDZN",
+    database: "dbreto",
+    port: 3307,
     connectionLimit: 10
 })
 
@@ -54,6 +55,64 @@ const limiter = rateLimit({
     max: 5,
     message: { success: false, message: "Demasiados intentos fallidos. Intenta otra vez en 7 minutos." }
 });
+
+
+app.post("/verify", (req, res) => {
+  const { correo, contrasena } = req.body;
+  console.log("Received request:", req.body);
+
+  if (!correo || !contrasena) {
+      return res.status(400).json({ message: "Favor de llenar todos los campos." });
+  }
+
+  const query = "SELECT contrasena, tipo FROM usuarios WHERE correo = ?";
+
+  pool.query(query, [correo], (err, results) => {
+      if (err) {
+          console.error(err);
+          return res.status(500).json({ success: false, message: "Error en Database." });
+      }
+
+      if (results.length === 0) {
+          return res.status(401).json({ success: false, message: "Correo o Contraseña Inválida." });
+      }
+
+      const hashedPassword = results[0].contrasena;
+      const tipoUsuario = results[0].tipo;
+
+      bcrypt.compare(contrasena, hashedPassword, (err, match) => {
+          if (err) {
+              console.error(err);
+              return res.status(500).json({ success: false, message: "Error verificando contraseña." });
+          }
+
+          if (match) {
+              req.session.user = { correo, tipo: tipoUsuario };
+
+              let redirectPath;
+              switch (tipoUsuario) {
+                  case "usuario":
+                      redirectPath = "/coursescreen.html";
+                      break;
+                  case "capacitador":
+                      redirectPath = "/curso_tecnicos.html";
+                      break;
+                  case "supervisor":
+                      redirectPath = "/vistaAdministrador.html";
+                      break;
+                  default:
+                      return res.status(403).json({ success: false, message: "Tipo de usuario no permitido." });
+              }
+
+              return res.redirect('http://zwwk4ocg8k0ko4g08wkgoo00.4.172.252.35.sslip.io${redirectPath}');
+          } else {
+              res.status(401).json({ success: false, message: "Correo o Contraseña Inválida." });
+          }
+      });
+  });
+});
+
+
 app.use("/verify", limiter);
 
 // ruta principal para capacitatec.whirlpool.com (index.html)
@@ -119,5 +178,5 @@ app.get("/logout", (req, res) => {
   // Servidor
 const PORT = process.env.PORT||3000;
 app.listen(PORT, () => {
-    console.log(`SERVER STARTING ON ${PORT}`)
+    console.log('SERVER STARTING ON ${PORT}')
 })
