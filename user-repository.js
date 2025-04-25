eae8cc045b184f8ca5e5cd3aaf99c4582cf1840a
Jaime Gamez <a01410192@tec.mx>
2025-04-24 20:17:58 -0600

    Autenticación de usuario

---
+++ user-repository.js
@@ -0,0 +1,28 @@
+import dbLocal from "db-local";
+const { Schema } = new dbLocal({path: './db'})
+
+const User = Schema('User', {
+    _id: { type: 'string', required: true },
+    username: { type: 'string', required: true},
+    password: { type: 'string', required: true },
+})
+
+export class UserRepository {
+    static create ({usernmae, password}){
+        //validate username 
+        if (typeof username !== 'string') {
+            throw new Error('username must be a string')
+        }
+        if (username.length < 3) {
+            throw new Error('username must be at least 3 characters long')
+        }
+
+        if (typeof password !== 'string') {
+            throw new Error('password must be a string')
+        }
+        if (password.length < 6) {
+            throw new Error('password must be at least 6 characters long')
+        }
+    }
+    static login ({username, password}){}
+}
