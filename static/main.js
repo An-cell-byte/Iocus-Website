5cebb7587991665584b2d8891a4f56baf8f65cdd
mbrgs240 <a00838607@tec.mx>
2025-04-26 14:45:17 -0600

    react

---
+++ static/main.js
@@ -0,0 +1,4 @@
+import './css/app.css'; // Si tienes CSS
+import './js/app.js'; // Tu JS antiguo (opcional)
+import "./css/app.css.map"; // Mapa de CSS (opcional)
+import "./js/app.js.map"; // Mapa de JS (opcional)
