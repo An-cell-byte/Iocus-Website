b390327c0067fe4f1d9344e436b9e2ccb7a10d27
Manuel Arias Alcocer <141689278+ManuelAriasAlcocer@users.noreply.github.com>
2025-04-27 22:00:05 -0600

    Update reference_quiz.js

--- videojuego/reference_quiz.js
+++ videojuego/reference_quiz.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8100;
 
 // Ruta para obtener el archivo JSON dinámicamente
 app.get("/quiz/:id", (req, res) => {
