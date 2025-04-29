c10ee0c5d0801e9f1b910b4ecfc6ce865f8c8d49
mbrgs240 <a00838607@tec.mx>
2025-04-28 18:15:18 -0600

    solve

--- static/vista-administrador/main.jsx
+++ static/vista-administrador/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CursoGrid from "./componentes/CursoGrid.jsx"; // el componente que haremos enseguida
+import CursoGrid from "./componentes/CursoGrid.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/app.css"; // si ya tienes tu propio css
+import "./css/app.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  console.log("Hola desde React"),
+console.log("Hola desde React"); // 👉 1) mensaje a consola
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // 👉 2) UN solo argumento: el árbol React
   <React.StrictMode>
     <CursoGrid />
   </React.StrictMode>
