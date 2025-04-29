8a9929fed92f0b45abe0e01fa502014fc0e3133f
mbrgs240 <a00838607@tec.mx>
2025-04-28 19:29:45 -0600

    build wit vite

--- static/main.js
+++ static/main.js
@@ -1,4 +1,2 @@
-import './css/app.css'; // Si tienes CSS
-import './js/app.js'; // Tu JS antiguo (opcional)
-import "./css/app.css.map"; // Mapa de CSS (opcional)
-import "./js/app.js.map"; // Mapa de JS (opcional)
+import "./css/app.css"; // Si tienes CSS
+import "./js/app.js"; // Tu JS antiguo (opcional)
