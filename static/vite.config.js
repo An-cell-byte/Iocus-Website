751d3e8bdc7f2541130863898041919331ca531a
Mario Burgos <a00838607@tec.mx>
2025-04-29 04:07:19 -0600

    vite config

---
+++ static/vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        admin: resolve(
+          __dirname,
+          "vista-administrador/vistaAdministrador.html"
+        ),
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
